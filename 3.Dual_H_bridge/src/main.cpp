7d5a76b303194c00311a4a2b9afd10e21f46ad44
fythatthepce <fythatthepce@gmail.com>
2017-07-04 12:06:55 +0700

    update

--- 3.Dual_H_bridge/src/main.cpp
+++ 3.Dual_H_bridge/src/main.cpp
@@ -2,24 +2,47 @@
 
 void setup()
 {
+     //Moter A
      pinMode(9,OUTPUT);   //ENA Analog 9
      pinMode(6,OUTPUT);   //IN1
      pinMode(7,OUTPUT);   //IN2
+
+
+     //Moter B
+
 }
 
 
 void loop()
 {
-     digitalWrite(6,HIGH);      //ตามตารางข้างบน ต้องกำหนด IN1 = HIGH
-     digitalWrite(7,LOW);       //และ IN2 = LOW มอเตอร์ A จึงจะทำงานหมุนไปด้านหน้า
-     analogWrite(9,255);       //และสั่งหมุนที่ความเร็วสูงสุด 255 ผ่านทาง ENA (ขา 10) ที่เป็น PWM
+     //motor move Forward with speed
+     digitalWrite(6,HIGH);
+     digitalWrite(7,LOW);
+     analogWrite(9,255);   //speed 255
+     delay(3000);          //3 sec
 
-     delay(3000);                     //รอเวลา 3 วินาที
 
 
-     digitalWrite(6,HIGH);       //ตามตารางข้างบน ต้องกำหนด IN1 = HIGH
-     digitalWrite(7,HIGH);       //และ IN2 = HIGH มอเตอร์ A จึงจะหยุดการทำงานทันที (เบรค)
-     analogWrite(9,0);            //และสั่งความเร็วมอเตอร์ให้เป็น 0 ผ่านทาง ENA (ขา 10) ที่เป็น PWM
-     delay(3000);
+     //motor move backward with speed
+     digitalWrite(6,LOW);
+     digitalWrite(7,HIGH);
+     analogWrite(9,255);   //speed 255
+     delay(3000);          //3 sec
+
+
+
+     //stop motor with break
+     digitalWrite(6,HIGH);
+     digitalWrite(7,HIGH);
+     analogWrite(9,0);     //speed 0
+     delay(3000);          //3 sec
+
+
+
+     //stop motor without break
+     digitalWrite(6,LOW);
+     digitalWrite(7,LOW);
+     analogWrite(9,0);     //speed 0
+     delay(3000);          //3 sec
 
 }
