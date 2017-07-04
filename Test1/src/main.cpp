22bba9ad13c1381ea2f5d0a17e7d315fa0fd8d3d
fythatthepce <fythatthepce@gmail.com>
2017-07-04 09:19:29 +0700

    update

---
+++ Test1/src/main.cpp
@@ -0,0 +1,26 @@
+#include "Arduino.h"
+
+// Set variable
+#define a 10
+//int a = 10;
+
+void setup()
+{
+  // initialize LED digital pin as an output.
+  pinMode(a, OUTPUT);
+}
+
+void loop()
+{
+  // turn the LED on (HIGH is the voltage level)
+  digitalWrite(a, HIGH);
+
+  // wait for a second
+  delay(200);
+
+  // turn the LED off by making the voltage LOW
+  digitalWrite(a, LOW);
+
+   // wait for a second
+  delay(200);
+}
