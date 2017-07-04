43323077621132884df86bc85fe8e2e7ffe54cec
fythatthepce <fythatthepce@gmail.com>
2017-07-04 12:38:27 +0700

    fix

--- 3.Dual_H_bridge/src/main.cpp
+++ 3.Dual_H_bridge/src/main.cpp
@@ -3,17 +3,17 @@
 void setup()
 {
      //Moter A
-     pinMode(10,OUTPUT);   //ENA Analog 9
+     pinMode(10,OUTPUT);   //ENA Analog 10
      pinMode(2,OUTPUT);   //IN1
      pinMode(3,OUTPUT);   //IN2
 
 
-/*
+
      //Moter B
-     pinMode(10,OUTPUT);   //ENB Analog 10
-     pinMode(2,OUTPUT);   //IN3
-     pinMode(3,OUTPUT);   //IN4
-     */
+     pinMode(9,OUTPUT);   //ENB Analog 9
+     pinMode(6,OUTPUT);   //IN3
+     pinMode(7,OUTPUT);   //IN4
+
 
 }
 
@@ -23,13 +23,13 @@ void loop()
 
      //MOTER A
      //motor move Forward with speed
-     digitalWrite(2,HIGH);
-     digitalWrite(3,LOW);
-     analogWrite(10,255);   //speed 255
+     digitalWrite(6,HIGH);
+     digitalWrite(7,LOW);
+     analogWrite(9,255);   //speed 255
      delay(3000);          //3 sec
 
 
-     /*
+
      //motor move backward with speed
      digitalWrite(6,LOW);
      digitalWrite(7,HIGH);
@@ -41,21 +41,14 @@ void loop()
      digitalWrite(7,LOW);
      analogWrite(9,0);     //speed 0
      delay(3000);          //3 sec
-     */
 
 
 
-     //stop motor with break
-     digitalWrite(2,HIGH);
-     digitalWrite(3,HIGH);
-     analogWrite(10,0);     //speed 0
-     delay(3000);          //3 sec
-  
 
 
 //-----------------------------------------------------------------------------
 
-/*
+
      //MOTER B
      //motor move Forward with speed
      digitalWrite(2,HIGH);
@@ -69,6 +62,6 @@ void loop()
      digitalWrite(3,HIGH);
      analogWrite(10,0);     //speed 0
      delay(3000);          //3 sec
-*/
+
 
 }
